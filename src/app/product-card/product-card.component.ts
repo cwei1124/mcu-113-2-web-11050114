9f07641f8790dad95d9ec376e517aaec7d6b117a
cwei1124 <eric2003921124@gmail.com>
2025-03-26 18:43:59 +0800

    feat(product): 利用屬性繫結實作圖片位址繫結

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -11,6 +11,8 @@ export class ProductCardComponent {
   author = '作者甲、作者乙、作者丙';
   company = '博碩文化';
 
+  photoUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
+
   isShow = true;
 
   onSetDisplay(isShow: boolean): void {
